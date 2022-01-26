ee92849485312f8bee7a41a855af92d260b07a42
Guadalupe Roldan <88412986+ashbrith@users.noreply.github.com>
2022-01-25 23:15:07 -0300

    Add files via upload
    Agregué las carpetas con imagenes default, y un array que las contiene.

---
+++ app/src/assets/images.js
@@ -0,0 +1,19 @@
+import defaultphoto01 from './img/defaultphoto01.png';
+import defaultphoto02 from './img/defaultphoto02.png';
+import defaultphoto03 from './img/defaultphoto03.png';
+import defaultphoto04 from './img/defaultphoto04.png';
+import defaultphoto05 from './img/defaultphoto05.png';
+import defaultphoto06 from './img/defaultphoto06.png';
+import defaultphoto07 from './img/defaultphoto07.png';
+import defaultphoto08 from './img/defaultphoto08.png';
+
+export default[
+  defaultphoto01,
+  defaultphoto02,
+  defaultphoto03,
+  defaultphoto04,
+  defaultphoto05,
+  defaultphoto06,
+  defaultphoto07,
+  defaultphoto08
+]
